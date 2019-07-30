0db6819189c50ff810400b1ef8d0a8bd44582cc3
summerlin <summerchihuilin@gmail.com>
2019-07-30 17:03:32 +0800

    rails task manager

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # READ
+  # get '/tasks', to: 'tasks#index', as: 'tasks'
+
+  # get 'tasks/new', to: 'tasks#new', as: 'new_task'
+  # get '/tasks/:id', to: 'tasks#show', as: 'task'
+  # # CREATE
+  # post 'tasks', to: 'tasks#create'
+
+  # # UPDATE
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  # patch 'tasks/:id', to: 'tasks#update'
+
+  # #DELETE
+  # delete '/tasks/:id', to: 'tasks#destroy'
+  resources :tasks
 end
+# we want to be able to destroy tasks directly from the list.
+# A JavaScript confirmation could be handy.
